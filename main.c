2813824251d288502640bf1d333e396de52b3050
NyRakath <Soncor23@gmail.com>
2021-12-08 18:44:45 -0500

    [FIX] Betty Styles

--- main.c
+++ main.c
@@ -7,55 +7,64 @@
  */
 int main(void)
 {
-	char *argument = NULL, *path = NULL, *string = NULL, **tokens = NULL;
+	char *string = NULL, **tokens = NULL;
 	size_t n_line = 0;
 	int a = 1, tty = 1, bytes_read;
-	int (*functionStr)(char **argv);
-	struct stat st;
 
-	if (isatty(STDIN_FILENO) == 0)
-		tty = 0;
+	isatty(STDIN_FILENO) == 0 ? tty = 0 : tty;
 	do
 
 	{
-		if (tty == 1)
-		{
-			write(out, "$h3llc1t4 ", 12);
-		}
+		tty == 1 ? write(out, "$h3llc1t4 ", 12) : tty;
 		fflush(stdin);
 		bytes_read = getline(&string, &n_line, stdin);
 		if (bytes_read == -1)
 		{
 			if (isatty(STDIN_FILENO))
+			{
 				printf("\n");
-			/*write("\n", 1);*/
+				free(string);
+			}
 			return (-1);
 		}
 		tokens = _strtok(string, " \n\t");
-		if (stat(tokens[0], &st) != 0)
-		{
-			functionStr = built_in(tokens);
-			if (functionStr == NULL)
-			{
-				path = _getenv("PATH");
-				argument = _tokenized_path(path, tokens[0]);
-				free(path);
-				if (argument == NULL)
-				{
-					perror("Error:");
-					continue;
-				}
-				execut(argument, tokens);
-			}
-			else
-				a = functionStr(tokens);
-		}
-		else
-		{
-			argument = tokens[0];
-			execut(argument, tokens);
-		}
-		free(string);
+		if (tokens[0] == NULL)
+			free(tokens);
+		validate_main(tokens);
+		free_arrays(tokens);
 	} while (a);
 	return (0);
+}
+
+/**
+ * validate_main - execute command
+ * @tokens: command tokenized
+ * Return: 1 to succed, -1 if fail
+ */
+char **validate_main(char **tokens)
+{
+	int (*functionStr)(char **argv);
+	struct stat st;
+	char *argument = NULL, **path = NULL;
+
+	if (stat(tokens[0], &st) != 0)
+	{
+		functionStr = built_in(tokens);
+		if (functionStr == NULL)
+		{
+			path = _getenv("PATH");
+			argument = _tokenized_path(path[1], tokens[0]);
+			if (argument == NULL)
+				perror("Error:");
+			execut(argument, tokens);
+		}
+		else
+			functionStr(tokens);
+	}
+	else
+	{
+		argument = tokens[0];
+		execut(argument, tokens);
+	}
+	return (tokens);
 }
